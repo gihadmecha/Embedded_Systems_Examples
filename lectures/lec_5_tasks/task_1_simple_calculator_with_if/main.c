a4e442a98eee951dfc219b149a1d19e7786ea806
gihadmecha <gihadmecha@gmail.com>
2021-08-02 06:52:04 +0200

    initialize_lec_5_tasks

---
+++ lectures/lec_5_tasks/task_1_simple_calculator_with_if/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned int answer = 0;
+
+    while(1)
+    {
+        //Initialize variables
+        int number_1 = 0;
+        int number_2 = 0;
+        char operator_sign ;
+
+        //Enter number
+        //printf("Enter a positive integer number: ");
+        scanf("%d", &number_1);
+        //printf("The first number = %d \n",number_1);
+
+        //Enter number
+        //printf("Enter an operator: ");
+        scanf(" %c", &operator_sign);
+        //printf("The operator = %c \n",operator_sign);
+
+         //Enter number
+        //printf("Enter another positive integer number: ");
+        scanf("%d", &number_2);
+        //printf("The second number = %d \n",number_2);
+
+        //the operation
+        if(operator_sign == '+')
+        {
+           answer = number_1 + number_2;
+           //print the answer
+           printf("The answer = %d \n", answer);
+        }
+        else if(operator_sign == '-')
+        {
+           answer = number_1 - number_2;
+           //print the answer
+           printf("The answer = %d \n", answer);
+        }
+        else if(operator_sign == '*')
+        {
+           answer = number_1 * number_2;
+           //print the answer
+           printf("The answer = %d \n", answer);
+        }
+        else if(operator_sign == '/')
+        {
+           answer = number_1 / number_2;
+           //print the answer
+           printf("The answer = %d \n", answer);
+        }
+        else
+        {
+           printf("Syntax Error \n");
+        }
+    }
+    return 0;
+}
